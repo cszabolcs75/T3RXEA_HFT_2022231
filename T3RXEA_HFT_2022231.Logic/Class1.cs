550f641f1e1e4106457872a8694b9c5705e2ddde
T3RXEA <cszabolcs75@gmail.com>
2022-10-12 19:03:24 +0200

    Solutions were created.

---
+++ T3RXEA_HFT_2022231.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace T3RXEA_HFT_2022231.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ T3RXEA_HFT_2022231.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace T3RXEA_HFT_2022231.Test
+{
+    public class Class1
+    {
+    }
+}
