9c672af3e2e43941ac50aed9b36fc45425170c28
T3RXEA <cszabolcs75@gmail.com>
2022-11-20 11:49:40 +0100

    Skeleton Repository is done.

---
+++ T3RXEA_HFT_2022231.Repository/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3RXEA_HFT_2022231.Repository
+{
+    abstract public class Repository<T>: IRepository<T> where T: class
+    {
+        protected ShoeManamegementDBContext ctx;
+
+        public Repository(ShoeManamegementDBContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public void Create(T entity)
+        {
+            ctx.Set<T>().Add(entity);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            ctx.Set<T>().Remove(entity);
+            ctx.SaveChanges();
+        }
+
+        public abstract T GetOne(int Id);
+    }
+}
