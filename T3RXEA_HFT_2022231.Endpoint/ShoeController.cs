54f6b7c4955f6d6071bb46249fc19fb61394e502
T3RXEA <cszabolcs75@gmail.com>
2022-11-25 17:02:25 +0100

    ShoeController done

---
+++ T3RXEA_HFT_2022231.Endpoint/ShoeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using T3RXEA_HFT_2022231.Logic;
+using T3RXEA_HFT_2022231.Models;
+
+namespace T3RXEA_HFT_2022231.Endpoint
+{
+    [Route("[controller")]
+    [ApiController]
+    public class ShoeController : ControllerBase
+    {
+        IShoeLogic sl;
+
+        public ShoeController(IShoeLogic sl)
+        {
+            this.sl = sl;
+        }
+
+        [HttpGet]
+        public IEnumerable<Shoe> Get()
+        {
+            return sl.ReadAllShoe();
+        }
+        [HttpGet("{id}")]
+        public Shoe Get(int id)
+        {
+            return sl.ReadShoe(id);
+        }
+        [HttpPost]
+        public void Post([FromBody] Shoe shoe)
+        {
+            sl.CreateShoe(shoe.Id, shoe.BrandId, shoe.SportId, shoe.Prize, shoe.Name);
+        }
+        [HttpPut]
+        public void Put([FromBody] Shoe shoe)
+        {
+            sl.UpdateShoe(shoe.Id, shoe.BrandId, shoe.SportId, shoe.Prize, shoe.Name);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            sl.DeleteShoe(id);
+
+        }
+    }
+}
