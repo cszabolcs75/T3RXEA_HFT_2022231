6d4e9028ec3f7739d2b8c6eb77f9cd27a8162c52
T3RXEA <cszabolcs75@gmail.com>
2022-11-25 17:01:09 +0100

    EndPoint configured.

--- T3RXEA_HFT_2022231.Endpoint/Startup.cs
+++ T3RXEA_HFT_2022231.Endpoint/Startup.cs
@@ -7,18 +7,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using T3RXEA_HFT_2022231.Logic;
+using T3RXEA_HFT_2022231.Repository;
 
 namespace T3RXEA_HFT_2022231.Endpoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddTransient<ShoeManamegementDBContext, ShoeManamegementDBContext>();
+            services.AddTransient<IBrandLogic, BrandLogic>();
+            services.AddTransient<IBrandRepository, BrandRepository>();
+            services.AddTransient<IShoeLogic, ShoeLogic>();
+            services.AddTransient<IShoeRepository, ShoeRepository>();
+            services.AddTransient<ISportLogic, SportLogic>();
+            services.AddTransient<ISportRepository, SportRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -26,14 +34,12 @@ namespace T3RXEA_HFT_2022231.Endpoint
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(x => x.AllowCredentials().AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:2286"));
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
