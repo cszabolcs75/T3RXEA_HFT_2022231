7316901882f338ea61ece05af7106d45657ade71
T3RXEA <cszabolcs75@gmail.com>
2023-05-17 22:40:45 +0200

    Js get added.

---
+++ GUIFeladat.JSClient/wwwroot/script.js
@@ -0,0 +1,12 @@
+﻿let shoes = [];
+
+function display() {
+    document.getElementById('resultarea').innerHTML = "";
+    shoes.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.shoeId + "</td><td>"
+            + t.shoeName + "</td></tr>";
+
+
+    });
+}
