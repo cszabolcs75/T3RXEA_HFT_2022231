3d30178c94c1a65ecfa307f9f0601d382f0b4688
T3RXEA <cszabolcs75@gmail.com>
2023-05-19 21:49:09 +0200

    Post is created.

--- GUIFeladat.JSClient/wwwroot/script.js
+++ GUIFeladat.JSClient/wwwroot/script.js
@@ -1,5 +1,19 @@
 ﻿let shoes = [];
 
+
+getdata();
+
+
+async function getdata() {
+    await fetch('http://localhost:2286/shoe')
+        .then(x => x.json())
+        .then(y => {
+            shoes = y;
+            console.log(shoes);
+            display();
+        });
+}
+
 function display() {
     document.getElementById('resultarea').innerHTML = "";
     shoes.forEach(t => {
@@ -10,3 +24,22 @@ function display() {
 
     });
 }
+
+function create() {
+    let name = document.getElementById('shoename').value;
+    fetch('http://localhost:2286/shoe', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { shoeName: name })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+   
+
+}
+
